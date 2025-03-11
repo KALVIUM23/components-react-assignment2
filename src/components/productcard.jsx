3b2c02ea3263282dd39a6a6bbc2cc1316c4ddf8c
KALVIUM23 <rockstarsagarkv206@gmail.com>
2025-03-11 14:15:39 +0530

    assigment2

--- src/components/productcard.jsx
+++ src/components/productcard.jsx
@@ -1,1 +1,25 @@
 // write product card here
+import React from 'react';  
+import Button from './button'; 
+import './productcard.css';  
+
+const Productcard = () => { 
+    const data = { 
+        productImage: 'https://res.cloudinary.com/dbs8l6w93/image/upload/v1741682067/cld-sample-5.jpg', 
+        productName: 'DJI Ozmo', 
+        price: '4500$'
+    }
+    
+  return (  
+    <div className="productcard">  
+      <img src={data.productImage} alt={data.productName} className="productcard-image" />  
+      <div className="productcard-details">  
+        <h3 className="productcard-name">{data.productName}</h3>  
+        <p className="productcard-price">${data.price}</p>  
+        <Button text="View Product" onClick={() => alert(`Viewing ${data.productName}`)} />  
+      </div>  
+    </div>  
+  );  
+};  
+
+export default Productcard;  
